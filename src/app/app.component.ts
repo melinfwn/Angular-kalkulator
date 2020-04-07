2909684e05651c493fcae2c5ecaef48e74f335b0
melinfwn <melindasari425@gmail.com>
2020-04-07 20:37:52 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular';
+  title = 'Simple Calculator';
+ public bilangan1 : number
+ public bilangan2 : number
+ public codeValue : string
+ public listHasil : Array<number> = []
+
+ hasil : number
+ jumlah() : number {
+    this.hasil = this.bilangan1+this.bilangan2
+    return this.hasil
+ }
+ kurang() : number{
+  this.hasil =  this.bilangan1-this.bilangan2
+    return this.hasil
+ }
+ kali() : number {
+  this.hasil =  this.bilangan1*this.bilangan2
+   return this.hasil
+ }
+ bagi() : number{
+  this.hasil =  this.bilangan1/this.bilangan2
+   return this.hasil
+ }
+ operationList = [
+   {id : 1, name : "penjumlahan"},
+   {id : 2, name : "pengurangan"},
+   {id : 3, name : "perkalian"},
+   {id : 4, name : "pembagian"}]
+
+   public saveCode(e): void {
+    let name = e.target.value;
+    let list = this.operationList.filter(x => x.name === name)[0];
+    console.log(list.id);
+    if(list.name == "penjumlahan"){
+      this.jumlah()
+    } else if (list.name == "pengurangan"){
+      this.kurang()
+    } else if (list.name == "perkalian") {
+      this.kali()
+   } else if (list.name == "pembagian") {
+     this.bagi()
+   }
+  }
+  cetak(){
+    this.listHasil.push(this.hasil)
+ }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {AccordionModule} from 'primeng/accordion';     //accordion and accordion tab
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';    
+import {ScrollPanelModule} from 'primeng/scrollpanel';
+import {ButtonModule} from 'primeng/button';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    AccordionModule,
+    BrowserAnimationsModule,
+    ScrollPanelModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
